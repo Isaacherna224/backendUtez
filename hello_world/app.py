29c43577525ea13f728faab8e98bc66fe727ec23
Elielo03 <elielorgz03@gmail.com>
2024-05-25 07:25:57 -0600

    fix(project new): delete codeSmell
    code smell in app.py was deleted

--- hello_world/app.py
+++ hello_world/app.py
@@ -1,7 +1,5 @@
 import json
 
-# import requests
-
 
 def lambda_handler(event, __):
     print(event)
@@ -12,7 +10,8 @@ def lambda_handler(event, __):
     event: dict, required
         API Gateway Lambda Proxy Input Format
 
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/
+        set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
 
     context: object, required
         Lambda Context runtime methods and attributes
@@ -23,16 +22,9 @@ def lambda_handler(event, __):
     ------
     API Gateway Lambda Proxy Output Format: dict
 
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/
+        set-up-lambda-proxy-integrations.html
     """
-
-    # try:
-    #     ip = requests.get("http://checkip.amazonaws.com/")
-    # except requests.RequestException as e:
-    #     # Send some context about this error to Lambda Logs
-    #     print(e)
-
-    #     raise e
 
     return {
         "statusCode": 200,
