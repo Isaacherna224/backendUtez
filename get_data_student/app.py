7cc8c806bfc3ed2f74037f31dca47dd6d84db9fc
Isaac Hernandez <isaac.hernandez.flrs@gmai.com>
2024-05-27 19:50:20 -0600

    refactor(lambda): add lambda text
    add lambda text

--- get_data_student/app.py
+++ get_data_student/app.py
@@ -6,6 +6,6 @@ def lambda_handler(event, __):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "get Data Students",
+            "message": "get Data Students lambda",
         }),
     }
